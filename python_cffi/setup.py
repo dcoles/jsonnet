4e64cf1f6b826517df26a3e7e28061479772cd32
David Coles <coles.david@gmail.com>
2020-04-12 20:34:37 -0700

    python: Don't include tests in package

--- python_cffi/setup.py
+++ python_cffi/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jsonnet',
-    packages=find_packages(),
+    packages=find_packages(exclude=['jsonnet.tests']),
     setup_requires=['cffi>=1.0.0'],
     cffi_modules=['jsonnet/build.py:ffibuilder'],
     install_requires=['cffi>=1.0.0'],
